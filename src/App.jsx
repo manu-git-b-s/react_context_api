9136f283ea836e26bc53731711b264c351a3ef43
Manupriyan <manupriyan722@gmail.com>
2024-02-17 19:49:53 +0530

    navbar styling completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Store from "./pages/Store.jsx";
+import About from "./pages/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
     </>
   );
 }
